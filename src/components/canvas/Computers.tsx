a4a882cb9cab03b5c2564187d5c50e70ed14f556
Nirav Chavda <nmchavda99@gmail.com>
2023-10-08 16:05:13 +0530

    Add mobile friendly Computer Model

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,9 +1,9 @@
 import {Canvas} from '@react-three/fiber';
 import {OrbitControls, Preload, useGLTF} from '@react-three/drei';
-import {Suspense} from "react";
+import {Suspense, useEffect, useState} from "react";
 import Loader from "../Loader";
 
-const Computers = () => {
+const Computers = ({isMobile}: { isMobile: boolean }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
@@ -18,8 +18,8 @@ const Computers = () => {
             shadow-mapSize={1025}
         />
         <primitive object={computer.scene}
-                   scale={0.75}
-                   position={[0, -3.25, -1.5]}
+                   scale={isMobile ? 0.7 : 0.75}
+                   position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
                    rotation={[-0.01, -0.2, -0.1]}
         />
       </mesh>
@@ -27,6 +27,23 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, []);
+
   return (
       <Canvas
           frameloop="demand"
@@ -40,7 +57,7 @@ const ComputersCanvas = () => {
                          minPolarAngle={Math.PI / 2}
           />
         </Suspense>
-        <Computers/>
+        <Computers isMobile={isMobile}/>
 
         <Preload all/>
       </Canvas>
