280791197ac59a0052a611135e641d07118b567d
Nirav Chavda <nmchavda99@gmail.com>
2023-10-08 13:02:14 +0530

    Add Initial Components

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+import {BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas} from './canvas';
+import Hero from './Hero.js';
+import Navbar from './Navbar';
+import About from './About.js';
+import Tech from './Tech';
+import Experience from './Experience.js';
+import Works from './Works';
+import Feedbacks from './Feedbacks.js';
+import Contact from './Contact.js';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas
+};
